340e75105913bbdc34398e744e2750ec111b6dbb
riedadr@zacharias <git@riedadr.dev>
2023-08-22 16:49:15 +0200

    use env

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
 $config = array(
-	"appcode" => "",
-	"ubi-email" => "",
-	"ubi-password" => "",
-	"default-region" => "emea", // Regions: emea - ncsa - apac 
+	"appcode" => getenv("APP_CODE"),
+	"ubi-email" => getenv("UBI_EMAIL"),
+	"ubi-password" => getenv("UBI_PASS"),
+	"default-region" => "emea", // Regions: emea - ncsa - apac
 	"default-stats" => "casualpvp_death,casualpvp_kdratio,casualpvp_kills", // All stats: https://gist.github.com/sidelux/c2724e64acb7e1b8921c11572800f8d4
 	"default-platform" => "uplay", // Platforms: uplay - xbl - psn
 	"default-progression" => "true", // Enabled: true - false
